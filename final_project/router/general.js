3241a6fc9feb0158691b34e3ff68355086132397
Htet-Wy <htetwaiyan2101@gmail.com>
2025-07-06 10:35:51 -0400

    added function for task 5

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,10 +4,20 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-
 public_users.post("/register", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+  if (!username || !password) {
+    return res.status(400).json({message: "Username and password are required"})
+  }
+
+  const userExists = users.filter(user => user.username === username);
+  if(!userExists) {
+    return res.status(409).json({message: "Username already exists."})
+  } 
+    users.push({username, password});
+  return res.status(200).json({message: "User registered successfully."});
 });
 
 // Get the book list available in the shop
